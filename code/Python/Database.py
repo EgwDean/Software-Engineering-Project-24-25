27ab7f41f62a9c387aed6064110a1f8d08c9b519
Konstantinos Anastasopoulos <k.matsaniaou2003@gmail.com>
2025-05-02 11:43:39 +0300

    Simple database class, amin file which connects to the database and a password file to store the password.

---
+++ code/Python/Database.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import mysql.connector
+
+class Database:
+    host = 'localhost'
+    user = 'root'
+    database = 'car_rental'
+    password_file = Path(__file__).parent.parent / 'db_password.txt'
+    connection = None
+
+    @classmethod
+    def connect(cls):
+        try:
+            password = cls.password_file.read_text(encoding='utf-8').strip()
+
+            cls.connection = mysql.connector.connect(
+                host=cls.host,
+                user=cls.user,
+                password=password,
+                database=cls.database
+            )
+
+            if cls.connection.is_connected():
+                print("Successfully connected to the database.")
+
+        except FileNotFoundError:
+            print(f"Password file not found at {cls.password_file}")
+        except mysql.connector.Error as e:
+            print(f"Error while connecting to MySQL: {e}")
+
+    @classmethod
+    def close(cls):
+        if cls.connection and cls.connection.is_connected():
+            cls.connection.close()
+            print("MySQL connection closed.")
---
+++ code/Python/main.py
@@ -0,0 +1,6 @@
+import Database
+
+# Connect to the database
+db = Database.Database()
+db.connect()
+db.close()
