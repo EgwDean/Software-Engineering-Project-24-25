6b37db30bdf7c6ef1c2a80d390910948fa64918f
Konstantinos Anastasopoulos <k.matsaniaou2003@gmail.com>
2025-05-02 21:20:32 +0300

    added mysql login procedure

---
+++ code/Database/procedures.sql
@@ -0,0 +1,34 @@
+USE car_rental;
+
+DELIMITER $
+DROP PROCEDURE IF EXISTS login$
+CREATE PROCEDURE login(IN username VARCHAR(50), IN password VARCHAR(50), OUT type VARCHAR(50))
+BEGIN 
+	DECLARE name VARCHAR(50);
+    DECLARE pass VARCHAR(50);
+    DECLARE user_count INT;
+	DECLARE admin_count INT;
+    
+    SELECT COUNT(*)
+    INTO user_count
+    FROM user
+    WHERE name = username AND pass = password;
+    
+    SELECT COUNT(*)
+    INTO admin_count
+    FROM admin
+    WHERE name = username AND pass = password;
+
+	IF (user_count > 0) THEN
+		SET type = 'user';
+	END IF;
+    
+	IF (admin_count > 0) THEN
+		SET type = 'admin';
+	END IF;
+    
+	IF (user_count = 0 AND admin_count = 0) THEN
+		SET type = 'NONE';
+	END IF;
+END$
+DELIMITER ;
